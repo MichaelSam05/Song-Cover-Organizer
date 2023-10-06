d59f9d67fd4ccda32864a0734a1d981d7011eaed
Michael Sam <michael2g1317@gmail.com>
2023-10-05 17:36:40 -0700

    Did Task 1 (Phase 1)

---
+++ src/main/model/Profile.java
@@ -0,0 +1,6 @@
+package model;
+
+//Represents a profile for a person having a username and password
+public class Profile {
+    //stub
+}
---
+++ src/main/model/Song.java
@@ -0,0 +1,7 @@
+package model;
+
+// Represents a song having a song name, artist name, views, likes, dislikes, instrument, date (dd/mm/yyyy)
+// and isFavorite
+public class Song {
+    //stub
+}
