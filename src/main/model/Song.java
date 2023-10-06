9e5a6c23ab496cb8ad11e788d6eed81f196a10ef
Michael Sam <michael2g1317@gmail.com>
2023-10-05 18:23:08 -0700

    Implemented addSong and getNumSongs

--- src/main/model/Song.java
+++ src/main/model/Song.java
@@ -3,5 +3,59 @@ package model;
 // Represents a song having a song name, artist name, views, likes, dislikes, instrument, date (dd/mm/yyyy)
 // and isFavorite
 public class Song {
-    //stub
+    private String songName;
+    private String artistName;
+    private int views;
+    private int likes;
+    private int dislikes;
+    private String instrument;
+    private String date;
+    private boolean isFavourite;
+
+    //REQUIRES: date formatted as (dd/mm/yyyy)
+    //EFFECTS: constructs a song with isFavorite set to false.
+    public Song(String songName, String artistName, int views, int likes, int dislikes, String instrument,
+                String date) {
+        this.songName = songName;
+        this.artistName = artistName;
+        this.views = views;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.instrument = instrument;
+        this.date = date;
+        this.isFavourite = false;
+    }
+
+    public String getSongName() {
+        return this.songName;
+    }
+
+    public String getArtistName() {
+        return this.artistName;
+    }
+
+    public int getViews() {
+        return this.views;
+    }
+
+    public int getLikes() {
+        return this.likes;
+    }
+
+    public int getDislikes() {
+        return this.dislikes;
+    }
+
+    public String getInstrument() {
+        return this.instrument;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public boolean getFavourite() {
+        return this.isFavourite;
+    }
+
 }
--- src/main/model/SongDatabase.java
+++ src/main/model/SongDatabase.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongDatabase {
-    //stub
+    private List<Song> songs = new ArrayList<>();
+
+    //EFFECTS: adds a song to the list
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+
+    //REQUIRES: num <= the size of the list
+    //EFFECTS: returns the first num elements in the list
+    public List<Song> getNumSongs(int num) {
+        if (songs.isEmpty()) {
+            return null;
+        } else {
+            List<Song> songList = new ArrayList<>();
+            for (int i = 0; i < num; i++) {
+                songList.add(songs.get(i));
+            }
+        }
+    }
 }
