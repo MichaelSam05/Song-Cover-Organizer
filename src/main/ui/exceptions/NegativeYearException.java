7fe377d08edb744de7746550436bffd503b6aff3
Michael Sam <michael2g1317@gmail.com>
2023-11-02 20:57:46 -0700

    Added exceptions

--- src/main/ui/exceptions/NegativeYearException.java
+++ src/main/ui/exceptions/NegativeYearException.java
@@ -4,6 +4,7 @@ public class NegativeYearException extends DateFormatException {
     public NegativeYearException() {
         super();
     }
+
     public NegativeYearException(String msg) {
         super(msg);
     }
